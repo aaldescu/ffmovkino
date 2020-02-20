097370b8ac867c88cf6a02893105abc5be9db4e4
aaldescu <aaldescu@users.noreply.github.com>
2020-02-20 09:10:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def hello(environ, start_response):
 	data = b"<h1>Hello</h1>, World!\n"
 	start_response("200 OK", [
-			("Content-Type", "text/plain"),
+			("Content-Type", "text/html; charset=UTF-8"),
 			("Content-Length", str(len(data)))
 	])
 	return iter([data])
